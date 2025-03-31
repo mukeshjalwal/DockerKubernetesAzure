cbfc1046bb1d8145542cb871090bc75502d83bda
Mukesh Jalwal <mukeshjalwal@Mukeshs-MacBook-Pro.local>
2025-03-31 22:06:03 +1100

    next version plust metadata update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/healthz', methods=['GET'])
 def health_check():
-    return jsonify({"status": "success", "version": "1.0.1"})
+    return jsonify({"status": "success", "version": "1.0.2"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
